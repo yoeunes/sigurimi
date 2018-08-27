1fb9d66af23d6f3e70e1e0262cc16490b1cf3a8b
subsider <ccsanroman@gmail.com>
2018-08-27 09:34:34 +0200

    Add sizable state to product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,8 +6,13 @@ $factory->define(App\Models\Product::class, function (Faker $faker) {
     return [
         'name'        => $faker->sentence,
         'description' => $faker->paragraph,
-        'size'        => $faker->randomElement(['S', 'M', 'L']),
         'price'       => $faker->numberBetween(100, 100000),
         'stock'       => $faker->numberBetween(0, 1000),
+    ];
+});
+
+$factory->state(App\Models\Product::class, 'sizable', function ($faker) {
+    return [
+        'size' => $faker->randomElement(['S', 'M', 'L']),
     ];
 });
